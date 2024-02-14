49fe3ff61c2a0ad2b7cf91fd848d79bd8d8df251
Frida Johannesson <fridasanneamanda@gmail.com>
2024-02-14 15:53:13 +0100

    Something happened

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-print("Hello! Welcome to a game of Pig!")
-print("           9")
-print("     ,--.-'-,--.")
-print("     \\  /-~-\\  /")
-print("    / )' a a `( \\")
-print("   ( (  ,---.  ) )")
-print("    \\ `(_o_o_)' /")
-print("     \\   `-'   /")
-print("      | |---| |  ")
-print("      [_]   [_]")
+import random
+
+
+def main():
+
+    print("Hello! Welcome to a game of Pig!")
+    print("           9")
+    print("     ,--.-'-,--.")
+    print("     \\  /-~-\\  /")
+    print("    / )' a a `( \\")
+    print("   ( (  ,---.  ) )")
+    print("    \\ `(_o_o_)' /")
+    print("     \\   `-'   /")
+    print("      | |---| |  ")
+    print("      [_]   [_]")
+
+    dice = random.randint(1, 6)
+    print(dice)
+
+    if __name__ == "__main__":
+        main()
