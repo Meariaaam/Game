7dac9559e6575a80961da6ece344cc0f1449ddef
Frida Johannesson <fridasanneamanda@gmail.com>
2024-02-15 16:46:11 +0100

    added classes

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
 
     import random
-
+  
     print("Hello! Welcome to a game of Pig!")
+    print("")
     print("           9")
     print("     ,--.-'-,--.")
     print("     \\  /-~-\\  /")
@@ -13,59 +14,66 @@ def main():
     print("      | |---| |  ")
     print("      [_]   [_]")
     print("")
-    print("Throw the dice!")
 
-    dice = random.randint(1, 6)
-    print(f"You got a {dice}!")
+    while True:
+        choice = input("Play -- Scores -- Exit\n")
 
-    if dice == 1:
-        print("_____________")
-        print("|           |")
-        print("|           |")
-        print("|     O     |")
-        print("|           |")
-        print("|___________|")
+        if choice == "play" or choice == "Play":
+            print("Throw the dice!")
+            dice = random.randint(1, 6)
+            print(f"You got a {dice}!")
+
+        elif dice == 1:
+            print("_____________")
+            print("|           |")
+            print("|           |")
+            print("|     O     |")
+            print("|           |")
+            print("|___________|")
         
-    elif dice == 2:
-        print("_____________")
-        print("|           |")
-        print("|        O  |")
-        print("|           |")
-        print("|  O        |")
-        print("|___________|")
+        elif dice == 2:
+            print("_____________")
+            print("|           |")
+            print("|        O  |")
+            print("|           |")
+            print("|  O        |")
+            print("|___________|")
 
-    elif dice == 3:
-        print("_____________")
-        print("|           |")
-        print("|        O  |")
-        print("|     O     |")
-        print("|  O        |")
-        print("|___________|")
+        elif dice == 3:
+            print("_____________")
+            print("|           |")
+            print("|        O  |")
+            print("|     O     |")
+            print("|  O        |")
+            print("|___________|")
+        elif dice == 4:
+            print("_____________")
+            print("|           |")
+            print("|  O     O  |")
+            print("|           |")
+            print("|  O     O  |")
+            print("|___________|")
 
-    elif dice == 4:
-        print("_____________")
-        print("|           |")
-        print("|  O     O  |")
-        print("|           |")
-        print("|  O     O  |")
-        print("|___________|")
+        elif dice == 5:
+            print("_____________")
+            print("|           |")
+            print("|  O     O  |")
+            print("|     O     |")
+            print("|  O     O  |")
+            print("|___________|")
 
-    elif dice == 5:
-        print("_____________")
-        print("|           |")
-        print("|  O     O  |")
-        print("|     O     |")
-        print("|  O     O  |")
-        print("|___________|")
+        elif dice == 6:
+            print("_____________")
+            print("|           |")
+            print("|  O     O  |")
+            print("|  O     O  |")
+            print("|  O     O  |")
+            print("|___________|")
 
-    elif dice == 6:
-        print("_____________")
-        print("|           |")
-        print("|  O    O   |")
-        print("|  O    O   |")
-        print("|  O    O   |")
-        print("|___________|")
+            if choice == "exit" or choice == "Exit":
+                break
 
 
 if __name__ == "__main__":
     main()
+
